9799478cd95cf00447927fa0e9d7a2d0aa90dccf
pmma_ <laurentSpam99@gmail.com>
2020-06-10 18:32:18 +0200

    infinity scroll kinda epic

--- public/js/albums.js
+++ public/js/albums.js
@@ -36,34 +36,55 @@ const app = new Vue({
                 username: "testUser",
                 thumbnail: "/public/img/uploads/example.jpg"
             }
-        ]
+        ],
+        noNewData: 0
     },
     components: {
         navbar
     },
     methods: {
         loadAlbums() {
-            axios.get(`/api/albums/${this.limit}/${this.offset}`)
-            .then(response => {
-                if(response.data.data.length > 0) {
-                    response.data.data.forEach(element => {
-                        element.thumbnail = "/public/img/uploads/" + element.thumbnail;
-                        this.albums.push(element);
-                    });
+            // Check if we received new data on the latest requests, else abort it
+            if(this.noNewData < 4) {
+                axios.get(`/api/albums/${this.limit}/${this.offset}`)
+                .then(response => {
+                    if(response.data.data.length > 0) {
+                        response.data.data.forEach(element => {
+                            element.thumbnail = "/public/img/uploads/" + element.thumbnail;
+                            this.albums.push(element);
+                        });
+    
+                        this.offset += this.limit;
+                        this.noNewData = 0;
+                    } else {
+                        this.noNewData++;
+                    }
+                })
+                .catch(error => {
+                    if(error.response) {
+                        Swal.fire(
+                            "Error!",
+                            error.response.data.message,
+                            "error"
+                        );
+                    }
+                });
+            }
+        },
+        inifintyScroll() {
+            window.onscroll = () => {
+                let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+            
+                if (bottomOfWindow) {
+                    if(!this.isLoading) {
+                        this.loadUrls();
+                    }
                 }
-            })
-            .catch(error => {
-                if(error.response) {
-                    Swal.fire(
-                        "Error!",
-                        error.response.data.message,
-                        "error"
-                    );
-                }
-            });
+            }
         }
     },
     mounted() {
         this.loadAlbums();
+        this.inifintyScroll();
     }
 });
